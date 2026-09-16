08ab19d140e930942d73a5ab9a0eef42d1049a8d
fundooboy9 <fundooboy9@gmail.com>
2026-09-16 12:15:44 +0530

    Create main.dart

---
+++ Lib/main.dart
@@ -0,0 +1,6 @@
+lib/screens/login_screen.dart
+​lib/screens/dashboard_screen.dart
+​lib/screens/ticket_scan_screen.dart
+​lib/screens/search_screen.dart
+​lib/screens/coach_result_screen.dart
+​lib/screens/platform_map_screen.dart
